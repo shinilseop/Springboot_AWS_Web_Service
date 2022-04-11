b042fd286f2e9d728144df4a0bda69dbd979759a
shinilseop <shinilseop12@gmail.com>
2022-04-12 02:02:58 +0900

    [Study] make Insert/Update/Select API

---
+++ src/main/java/com/Shins/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,30 @@
+package com.Shins.book.springboot.web.dto;
+
+import com.Shins.book.springboot.domain.posts.Posts;
+import com.Shins.book.springboot.posts.PostsService;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title=title;
+        this.content=content;
+        this.author=author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
