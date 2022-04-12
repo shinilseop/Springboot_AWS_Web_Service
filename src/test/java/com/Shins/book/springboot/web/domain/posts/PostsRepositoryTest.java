3013b906be0c79c27e243c216f144762af4b8444
shinilseop <shinilseop12@gmail.com>
2022-04-13 02:00:28 +0900

    [Study] JPA Auditing (createdTime, modifiedTime)

--- src/test/java/com/Shins/book/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/Shins/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -67,5 +68,26 @@ public class PostsRepositoryTest {
         System.out.println("POSTS 1 ::: " + posts1.getId());
         System.out.println("POSTS 2 ::: " + posts2.getId());
         System.out.println("POSTS 3 ::: " + posts3.getId());
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
