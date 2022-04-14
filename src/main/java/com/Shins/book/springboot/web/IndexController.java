20926ad4fc22d05e8eb8eb80fb448e12365e63e8
shinilseop <shinilseop12@gmail.com>
2022-04-15 00:56:08 +0900

    [Study] SpringSecurity Google OAuth2

--- src/main/java/com/Shins/book/springboot/web/IndexController.java
+++ src/main/java/com/Shins/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.Shins.book.springboot.web;
 
+import com.Shins.book.springboot.config.auth.LoginUser;
 import com.Shins.book.springboot.config.auth.dto.SessionUser;
 import com.Shins.book.springboot.posts.PostsService;
 import com.Shins.book.springboot.web.dto.PostsResponseDto;
@@ -9,19 +10,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
