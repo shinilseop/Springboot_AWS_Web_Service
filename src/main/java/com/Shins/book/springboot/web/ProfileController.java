a20ef427071c6f558a1014ce6c26cbeb041768c3
shinilseop <shinilseop12@gmail.com>
2022-04-25 17:13:38 +0900

    create ProfileController

---
+++ src/main/java/com/Shins/book/springboot/web/ProfileController.java
@@ -0,0 +1,29 @@
+package com.Shins.book.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.
+                getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "read1", "real2");
+
+        String defaultProfiles = profiles.isEmpty()? "default":profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfiles);
+    }
+}
