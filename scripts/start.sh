a8601c5571c2db55bad56245d3dbc5e176da5c60
shinilseop <shinilseop12@gmail.com>
2022-04-25 18:30:39 +0900

    Update start.sh [syntax error]

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
-soruce ${ABSDIR}/profile.sh
+source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=Springboot_AWS_Web_Service
